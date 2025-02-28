c98fb52a9cd1640cb7eab5fa35075021437645a0
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-02-28 21:27:33 +0530

    [Nitesh] ADD: Kilomter to miles

--- KmToMilesConverter.java
+++ KmToMilesConverter.java
@@ -1,15 +1,12 @@
 public class KmToMilesConverter {
     public static void main(String[] args) {
-        // Given distance in kilometers
+       
         double kilometers = 10.8;
         
-        // Conversion factor
         double conversionFactor = 1.6;
         
-        // Convert kilometers to miles
         double miles = kilometers / conversionFactor;
         
-        // Print the result
         System.out.println("The distance " + kilometers + " km in miles is " + miles);
     }
 }
