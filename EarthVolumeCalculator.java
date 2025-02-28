a34ed2c7fff288c366739cabb6bd2c4185c96029
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-02-28 22:01:31 +0530

    [Nitesh] ADD: calculating earth volume

---
+++ EarthVolumeCalculator.java
@@ -0,0 +1,12 @@
+public class EarthVolumeCalculator {
+    public static void main(String[] args) {
+        double radiusKm = 6378;
+        
+        double kmToMilesFactor = 0.239913;
+  
+        double volumeKm3 = (4.0 / 3.0) * Math.PI * Math.pow(radiusKm, 3);
+        double volumeMiles3 = volumeKm3 * kmToMilesFactor;
+        
+        System.out.println("The volume of Earth in cubic kilometers is " + volumeKm3 + " and in cubic miles is " + volumeMiles3);
+    }
+}
