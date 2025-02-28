3db786808158536c381dbd5d5e7c54c8fcf3b435
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-02-28 20:14:27 +0530

    [Nitesh] ADD :

---
+++ SamAverage.java
@@ -0,0 +1,13 @@
+class SamAverage{
+
+public static void main(String[] args){
+
+ int maths=94;
+ int physics=95;
+ int chemistry=96;
+
+System.out.print("Sam's average mark in PCM IS "+ (maths+physics+chemistry)/3 ); 
+ 
+  }
+
+}
