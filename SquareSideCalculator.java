17c3080be4517cc33ecd032394cbab8a69ac48d7
niteshxx <nitesh1136.be21@chitkarauniversity.edu.in>
2025-03-01 15:14:33 +0530

    [Nitesh] Add: perimeter to side

---
+++ SquareSideCalculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SquareSideCalculator {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+ 
+        System.out.print("Enter the perimeter of the square: ");
+        double perimeter = input.nextDouble();
+     
+        double side = perimeter / 4;
+        
+        System.out.println(String.format("The length of the side is %.2f whose perimeter is %.2f", side, perimeter));
+
+    
+    }
+}
